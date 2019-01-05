ac9ade6cd1be32052437e4a3ee071352c514d6d3
Kat B <katreesebarnes@gmail.com>
2019-01-05 19:53:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-   array.slice(inde)
+   array.slice(element)
    return array
   
 }
