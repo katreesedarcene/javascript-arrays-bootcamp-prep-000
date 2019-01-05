1a4da039e3cda5f95896c8d82283edaaac0ce850
Kat B <katreesebarnes@gmail.com>
2019-01-05 14:59:46 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var chocolateBars = ["snickers", "hundredgrand", "kitkat", "skittles"] 
+
+var cities = ["New York", "San Francisco"]
+
+["Philadelphia, ...cities"]
+
+cities
+
+
+var cities = ["New York", "San Francisco"];
+
+ [...cities, "Philadelphia"]
+ 
+ function addElementToBeginningOfArray(array, element) {
+   return [element, ...array]
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return [element, ...array]
+}
+ 
+
+
+
+
+
