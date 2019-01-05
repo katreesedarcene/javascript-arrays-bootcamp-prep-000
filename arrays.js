5325f4cf92b220c8d2a931fb09b98707fa6c442f
Kat B <katreesebarnes@gmail.com>
2019-01-05 21:20:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -52,7 +52,7 @@ function removeElementFromBeginningOfArray(array) {
    
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-    array.slice()
+    array.slice(4)
     return array
 }
 
