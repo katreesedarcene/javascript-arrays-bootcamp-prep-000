ff6523f7fca0677ee4f9824f8080d26d8e029abb
Kat B <katreesebarnes@gmail.com>
2019-01-05 16:59:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-   array.push, element
+   array.push(element)
    return array
 }
 
